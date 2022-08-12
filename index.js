6e9f580a72975750e7a176554778331082250b6b
kr1sto <91905647+montekristoo@users.noreply.github.com>
2022-08-12 22:10:57 +0300

    heroku

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ import {
 import checkAuth from "./utils/checkAuth.js";
 
 mongoose
-    .connect(process.env.MONGODB_URL)
+    .connect(
+        "mongodb+srv://kr1sto:1s2a3dqwer5@cluster0.t1pxavl.mongodb.net/quotes?retryWrites=true&w=majority" ||
+        process.env.MONGODB_URI
+    )
     .then(() => console.log("DB ok!"))
     .catch((e) => console.log("DB error!", e));
 
